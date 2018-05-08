878cfd9c916d0520f9a93538e2569961652a94b8
Shelly Yip <shelly@shellyyip.com>
2018-05-08 18:24:41 -0400

    Add css-loader, style-loader & new rule

--- webpack.config.js
+++ webpack.config.js
@@ -26,6 +26,10 @@ module.exports = {
           }
         },
       },
+      {
+        test: /\.(css|scss)$/,
+        loader: 'style-loader!css-loader?modules&importLoaders=1&localIdentName=[name]__[local]___[hash:base64:5]' 
+      }
     ]
   },
 
